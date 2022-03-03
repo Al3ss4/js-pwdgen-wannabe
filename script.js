61ba8bc0e3440faaf217ae1555e90433a1d9ef70
Mario Rovito <alessandro.r14@gmail.com>
2022-03-03 15:42:56 +0100

    fix code

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 // prompt per chiedere il nome
 var nome = prompt ( 'Qual è il tuo nome?');
 
-console.log(nome);
+//console.log(nome);
 
 // prompt per chiedere il nome
 var cognome = prompt ( 'Qual è il tuo cognome?');
 
-console.log(cognome);
+//console.log(cognome);
 
 
 // prompt per chiedere il colore preferito
 var colore = prompt ( 'Qual è il tuo colore preferito?');
 
-console.log(colore);
+//console.log(colore);
 
 // password (in)sicura
-console.log( nome + cognome + colore + 21);
+var password = ( nome + cognome + colore +21 ); 
+console.log( password )
 
 //password insicura HTML
-document.getElementById('psw').innerHTML = ' La tua password è ' + nome + cognome + colore + 21; 
+document.getElementById('psw').innerHTML = ' La tua password è ' + password ;
